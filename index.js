71a953528d23f4c50fcd2b44249b92c0c114b5e7
Hiten Kumar <hitenkumarvendani@gmail.com>
2022-10-08 15:24:51 -0700

    updated

--- index.js
+++ index.js
@@ -1,4 +1,50 @@
-let element = document.getElementsByClassName("discover").addEventListener("click",abc)
-function abc(){
-    document.getElementsByClassName("discover").style.color='blue';
-}
+<div class="middle1">
+<div class="Images">
+    <div class="image1">
+
+        <img src="a.png" alt=" " width="250 px " height="250 ">
+
+        <div class="img1text">
+            <p style="text-align:center;"><b>Robotics field involves the conception, design, manufacture and operation of robots. The objective of the robotics field is to create intelligent machines that can assist humans in a variety.</b></p>
+
+            <div class="img1link">
+                <b>    <a href="https://builtin.com/robotics" style="color: rgb(161, 136, 136) ;" target="_blank" rel="noopener noreferrer"> Click Here For More</a></b>
+
+
+            </div>
+        </div>
+    </div>
+    <div class="image2 ">
+        <img src="b.png " alt=" " width="250 px " height="250 " border-radius="25px">
+
+        <div class="img2text">
+            <p style="text-align:center;"><b>Artificial intelligence (AI) is the ability of a computer or a robot controlled by a computer to do tasks that are usually done by humans because they require human intelligence and discernment.</b> </p>
+
+            <div class="img2link">
+
+                <b> <a href="https://www.techtarget.com/searchenterpriseai/definition/AI-Artificial-Intelligence" style="color: rgb(161, 136, 136) ;" target="_blank" rel="noopener noreferrer"> Click Here For More</a></b>
+
+
+            </div>
+        </div>
+    </div>
+
+
+    <div class="image3 ">
+        <img src="c.png " alt=" " width="250 px " height="250 ">
+
+        <div class="img3text">
+            <p style="text-align:center;"> <b>Data science is the field of applying advanced analytics techniques and scientific principles to extract valuable information from data for business decision-making, strategic planning.</b> </p>
+
+            <div class="img3link">
+                <b><a href="https://www.simplilearn.com/tutorials/data-science-tutorial/what-is-data-science" style="color: rgb(161, 136, 136) ;" target="_blank" rel="noopener noreferrer">Click Here For More</a></b>
+
+
+            </div>
+
+        </div>
+    </div>
+
+    
+</div>
+</div>
